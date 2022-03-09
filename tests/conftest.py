4691e776efa38e1cb252d0e7c122fdab7c5cfcb9
Joao Victor Rocha <jvpereirarocha@gmail.com>
2022-03-08 21:47:36 -0300

    new mocks to test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,66 @@
+from typing import Optional
 import pytest
 from datetime import datetime
 from src.domain.model import Batch
+from tests.adapters.mock_repository import FakeRepository
+import random
+
+def mock_batch_generate(reference: str, sku: str, quantity: int, eta: Optional[datetime] = None):
+    
+    def make_mock():
+        if not reference:
+            reference = "ref-001"
+        if not sku:
+            sku = "TEST-PRODUCT"
+        if not quantity:
+            quantity = 10
+        
+        batch = Batch(
+            reference=reference,
+            sku=sku,
+            quantity=quantity,
+            eta=eta
+        )
+        return batch
+
+    yield make_mock
 
 
 @pytest.fixture(scope="function")
 def mock_batch_with_eta():
-    batch = Batch(
+    batch = mock_batch_generate(
         reference="ref-001",
         sku="TEST-PRODUCT",
         quantity=10,
         eta=datetime.now()
     )
     return batch
+
+
+def mock_list_of_batches():
+    list_of_batches = []
+    for i in range(0, 4):
+        mock_batch = mock_batch_generate(
+            reference=f"ref-00{i}",
+            sku=f"TEST-PRODUCT-{i}",
+            quantity=random.choice([x for x in range(0, 100)]),
+            eta=datetime(
+                year=2022,
+                month=1,
+                day=3
+            )
+        )
+        list_of_batches.append(mock_batch)
+    
+    return list_of_batches
+
+
+
+
+
+batches = mock_list_of_batches()
+
+@pytest.fixture
+def mock_fake_repository():
+    
+    return FakeRepository(batches=batches)
