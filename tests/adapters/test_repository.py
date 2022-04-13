23aa0612b659082712e6e2a7c431ae51c5ad9d26
Joao Victor <jvpereirarocha@gmail.com>
2022-04-12 22:50:04 -0300

    create new tests for all repository methods

--- tests/adapters/test_repository.py
+++ tests/adapters/test_repository.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from mock_repository import FakeRepository
 
 
@@ -5,3 +6,36 @@ def test_get_batch(mock_list_of_batches):
     batches = mock_list_of_batches
     repo = FakeRepository(batches)
     assert repo.get(reference="A001") == []
+
+
+def test_add_batch(mock_batch_generate):
+    batches = set()
+    batch = mock_batch_generate(
+        "REF0001",
+        "TABLE-010",
+        20,
+        datetime.now()
+    )
+    repo = FakeRepository(batches=batches)
+    repo.add(batch=batch)
+    assert len(repo.list()) > 0
+
+
+def test_remove_batch(mock_batch_generate):
+    # add a new batch, firstly
+    batches = set()
+    batch = mock_batch_generate()
+    repo = FakeRepository(batches)
+    repo.add(batch=batch)
+    assert len(repo.list()) > 0
+
+    # now, let's remove the batch
+    repo.remove(batch=batch)
+    assert len(repo.list()) == 0
+
+
+def test_list_all_batches(mock_list_of_batches):
+    batches = mock_list_of_batches
+    repo = FakeRepository(batches=batches)
+    list_of_batches = repo.list()
+    assert len(list_of_batches) == 4
